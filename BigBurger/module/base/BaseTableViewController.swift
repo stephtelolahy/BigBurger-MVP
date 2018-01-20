2dec667eb1381dca32d11a7759536b30d395a79a
Telolahy <stephano.telolahy@gmail.com>
2018-01-20 07:24:51 +0100

    Update BaseTableViewController

--- BigBurger/module/base/BaseTableViewController.swift
+++ BigBurger/module/base/BaseTableViewController.swift
@@ -10,15 +10,31 @@ import UIKit
 
 class BaseTableViewController: UITableViewController {
     
+    var presenter: (BasePresenter<BaseView>)?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        do {
+            self.presenter = try self.initPresenter()
+        } catch let error {
+            print(error)
+        }
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        
+        presenter?.onWillAppear()
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
+        
+        presenter?.onWillDisappear()
+    }
+    
+    func initPresenter() throws -> BasePresenter<BaseView>  {
+        throw NSError.init(domain: "Should implement InitPresenter \(BaseTableViewController.self)", code: 0, userInfo: nil)
     }
 }
